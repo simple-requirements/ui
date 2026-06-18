5e615608b18ae178a740d371696e7877b70d8237
Florian Weidinger <crasyhorse@gmx.net>
2026-06-18 09:52:08 +0000

    build: Implement Orval and Tanstack

---
+++ orval.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'orval';
+
+const openApiDocument = './openapi/backend-api.json';
+
+export default defineConfig({
+    backendApi: {
+        input: { target: openApiDocument },
+
+        output: {
+            client: 'react-query',
+            httpClient: 'fetch',
+            mode: 'tags-split',
+
+            target: './src/api/generated/endpoints',
+            schemas: './src/api/generated/models',
+
+            clean: true,
+            urlEncodeParameters: true,
+
+            override: {
+                query: { useQuery: true, useMutation: true, usePrefetch: true },
+
+                fetch: { includeHttpResponseReturnType: false, forceSuccessResponse: true },
+            },
+        },
+    },
+
+    backendApiZod: {
+        input: { target: openApiDocument },
+
+        output: {
+            client: 'zod',
+            mode: 'tags-split',
+
+            target: './src/api/generated/zod',
+            fileExtension: '.zod.ts',
+
+            clean: true,
+        },
+    },
+});

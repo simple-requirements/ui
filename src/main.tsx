88f6d4726133bad4bfde1be60a61ae509113c888
Florian Weidinger <crasyhorse@gmx.net>
2026-06-28 17:09:00 +0000

    chore: Import router config

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
 
-import App from '@/App';
+import { router } from '@/router';
 
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -12,6 +13,6 @@ import '@/main.scss';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 );
---
+++ src/router/index.ts
@@ -0,0 +1,5 @@
+import { createBrowserRouter, type RouteObject } from 'react-router';
+
+export const routes: RouteObject[] = [];
+
+export const router = createBrowserRouter(routes);

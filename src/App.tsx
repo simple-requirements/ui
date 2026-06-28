c387dd2f333dbd5074ce14961acb03f46fb4c414
Florian Weidinger <crasyhorse@gmx.net>
2026-06-28 18:32:28 +0000

    chore: Use QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { RootLayout } from '@/pages/RootLayout';
+import { RouterProvider } from 'react-router';
+
+import { router } from '@/router';
 
 function App() {
-    return <RootLayout />;
+    return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router';
 
-import { router } from '@/router';
+import App from '@/App';
+import { queryClient } from '@/api/queryClient';
 
 import 'primereact/resources/themes/lara-light-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
@@ -13,6 +14,8 @@ import '@/main.scss';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </StrictMode>,
 );

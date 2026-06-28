2d49b8ea577745b4d584aa82aeca2cd275314a43
Florian Weidinger <crasyhorse@gmx.net>
2026-06-28 17:03:37 +0000

    fix: Make app runnable

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+
     resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+
+    optimizeDeps: { entries: ['index.html'] },
+
     test: {
         include: ['test/**/*.spec.ts'],
         environment: 'jsdom',
@@ -18,4 +23,5 @@ export default defineConfig({
             exclude: ['test/**/*.spec.{ts,tsx}', 'src/main.tsx', 'src/**/*.d.ts'],
         },
     },
+    server: { host: '127.0.0.1', port: 5173 },
 });

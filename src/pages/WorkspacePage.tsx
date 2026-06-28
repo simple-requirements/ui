7bab1e0af9a4cf7c2f839bc5135cf7664e1b3c58
Florian Weidinger <crasyhorse@gmx.net>
2026-06-28 17:23:37 +0000

    Add demo root route

---
+++ src/pages/WorkspacePage.tsx
@@ -0,0 +1,10 @@
+export function WorkspacePage() {
+    return (
+        <div className='root-layout__content-placeholder'>
+            <h1 className='root-layout__content-title'>Workspace</h1>
+            <p className='root-layout__content-text'>
+                Placeholder content area. Project and requirement views will be rendered here later.
+            </p>
+        </div>
+    );
+}
